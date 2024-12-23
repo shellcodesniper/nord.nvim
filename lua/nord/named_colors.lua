783d1adf1bddbe30247d32b18f3984c2f79005b9
KuuwangE <root@ql.gl>
2024-12-23 11:21:49 +0900

    Update named_colors.lua

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,7 +1,7 @@
 local nord = {
 	--16 colors
 	black = "#2E3440", -- nord0 in palette
-	dark_gray = "#3B4252", -- nord1 in palette
+	dark_gray = "#c3c8d4", -- nord1 in palette
 	gray = "#434C5E", -- nord2 in palette
 	light_gray = "#4C566A", -- nord3 in palette
 	light_gray_bright = "#616E88", -- out of palette
